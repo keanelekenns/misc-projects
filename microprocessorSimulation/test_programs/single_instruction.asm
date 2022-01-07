ce86afa4febbab3ff0095478555c14ae8b08e442
Keanelek Enns <keanelekenns@gmail.com>
2022-01-07 15:04:00 -0800

    move microprocessor simulation to new repo

--- microprocessorSimulation/test_programs/single_instruction.asm
+++
@@ -1,8 +0,0 @@
-00000100 Add fifty seven to accumulator
-00111001
-00010100 subtract fifty seven to accumulator
-00111001
-01101000 jtz
-00000000
-00000000
-00000001 HLT
--- microprocessorSimulation/test_programs/single_instruction_tests/inr_dcr.asm
+++
@@ -1,5 +0,0 @@
-00010000 INC increment register c
-00010000 INC increment register c
-00010000 INC increment register c
-00010001 DCC decrement register c
-11111111 HLT
